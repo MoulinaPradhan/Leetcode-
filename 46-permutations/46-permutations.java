b000dd5ea8808973054ee2ad430de35ca1695d9d
Moulina Pradhan <pmoulina76@gmail.com>
2022-05-05 11:59:34 +0530

    Time: 3 ms (37.43%), Space: 45 MB (24.16%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,10 +1,10 @@
 class Solution {
     List<List<Integer>>res = new ArrayList<>();
     public List<List<Integer>> permute(int[] nums) {
-        fn(nums,0,new ArrayList<>());
+        fn(nums,new ArrayList<>());
         return res;
     }
-    public void fn(int [] nums, int idx, List<Integer> curr){
+    public void fn(int [] nums,List<Integer> curr){
         if(curr.size() == nums.length){
             res.add(new ArrayList<>(curr));
             return;
@@ -12,7 +12,7 @@ class Solution {
         for(int i=0;i<nums.length;i++){
                 if(curr.contains(nums[i])) continue;
             curr.add(nums[i]);
-            fn(nums, i,curr);
+            fn(nums,curr);
             curr.remove(curr.size() -1);
         }
     }
