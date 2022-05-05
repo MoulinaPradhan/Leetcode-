89a6f3c790b5fea4de87eae365626ffd80e93b48
Moulina Pradhan <pmoulina76@gmail.com>
2022-05-05 08:25:12 +0530

    Time: 22 ms (26.41%), Space: 188.9 MB (56.77%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -1,25 +1,28 @@
 class Solution {
-    List<List<String>>res = new ArrayList<>();
+    List<List<String>> res = new ArrayList<>();
     public List<List<String>> partition(String s) {
-     helper(s,0,new ArrayList<>());
+        fn(s,0, new ArrayList<>());
         return res;
-    }
-    public void helper(String s, int index, List<String>path){
-        if(index == s.length()){
-            res.add(new ArrayList<>(path));
+        
         }
-        for(int i=index;i<s.length();i++){
-            if(isPali(s,index,i)){
-                path.add(s.substring(index,i+1));
-                helper(s,i+1,path);
-                path.remove(path.size()-1);
+    public void fn(String s, int idx, List<String> curr){
+        if(idx == s.length()){
+            res.add(new ArrayList<>(curr));
+            return ;
+        }
+        for(int i=idx;i<s.length();i++){
+            if(pali(s, idx, i)){
+                curr.add(s.substring(idx,i+1));
+                fn(s, i+1, curr);
+                curr.remove(curr.size()-1);
             }
         }
     }
-    public boolean isPali(String s, int start, int end){
-       while(start<=end){
-           if(s.charAt(start++) != s.charAt(end--)) return false;
-       }
+    public boolean pali(String s , int start, int end){
+        while(start <= end){
+            if(s.charAt(start++) != s.charAt(end--)) return  false;
+            
+        }
         return true;
     }
 }
