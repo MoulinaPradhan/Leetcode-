8a3283f12fc1ce63f6fcf2473fb5183e01dab135
Moulina Pradhan <pmoulina76@gmail.com>
2022-05-08 16:57:39 +0530

    Time: 42 ms (66.35%), Space: 43.4 MB (53.55%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,27 +1,25 @@
 class Solution {
-   private int lo, maxLen;   //lo is used to track the starting index of the longest palindrome, maxLen is the length of the longest palindrome
+   private int lo, maxLen;  
 
 public String longestPalindrome(String s) {
 	int len = s.length();
 	if (len < 2)
-		return s; //if 0 or 1 chars, return the input string, no palindrome to be checked for
+		return s; 
 	
     for (int i = 0; i < len-1; i++) {
-     	extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible. example " aba"
-     	extendPalindrome(s, i, i+1); //assume even length. example "abba"
+     	extendPalindrome(s, i, i);  
+     	extendPalindrome(s, i, i+1); 
     }
-    return s.substring(lo, lo + maxLen); // return the part of the input string starting at the "lo" index, length of maxLen
+    return s.substring(lo, lo + maxLen); 
 }
 
 private void extendPalindrome(String s, int j, int k) { 
-	while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) { //check if the chars at indexes j and k are the same (condition for palindrome)
-		//while we have a palindrome, "extend" the length of the string being checked by decreasing the value of the beginning char index "j", 
-                //and increasing the value of ending char index "k"
+	while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) { 
                  j--; 
 		k++;
 	}
-	if (maxLen < k - j - 1) { //if the length of the current palindrome (k - j - 1) is greater than the previous longest palindrome
-		lo = j + 1; //store the beginning index of the palindrome
-		maxLen = k - j - 1; // and how long the string is. 
+	if (maxLen < k - j - 1) { 
+		lo = j + 1;
+		maxLen = k - j - 1; 
 	}
 }}
