4eae15eab8a202d31d9cf031424beff01682b049
Moulina Pradhan <pmoulina76@gmail.com>
2022-05-20 10:52:34 +0530

    Time: 202 ms (74.50%), Space: 193.4 MB (57.05%) - LeetHub

--- 1707-maximum-xor-with-an-element-from-array/1707-maximum-xor-with-an-element-from-array.java
+++ 1707-maximum-xor-with-an-element-from-array/1707-maximum-xor-with-an-element-from-array.java
@@ -1,75 +1,74 @@
-class Solution {
-   
-    class Trie {
-        Trie[] children;
-        public Trie() {
-            children = new Trie[2];
+class Node{
+    Node links[]= new Node[2];
+    boolean contain(int idx){
+        return links[idx] !=null;
+    }
+   Node get(int idx){
+       return links[idx];
+   } 
+    void put(int idx, Node node){
+        links[idx] = node;
+    }
+    }
+class Trie{
+     public Node root ;
+    Trie(){
+        root = new Node();
+    }
+    public void insert(int num){
+       Node node = root;
+        for(int i = 31;i>=0;i--) {
+            int bit = (num >> i) & 1; 
+            if(!node.contain(bit)) {
+                node.put(bit, new Node()); 
+            }
+            node = node.get(bit); 
         }
     }
-
-    Trie root;
-
-    public int[] maximizeXor(int[] nums, int[][] q) {
-
-        int n = nums.length;
-        int idx=0;
-        root = new Trie();
-        int[][] queries = new int[q.length][3];
-
-        for(int i=0;i<q.length;++i){
-            queries[i][0] = q[i][0];
-            queries[i][1] = q[i][1];
-            queries[i][2] = i;
+    public int getMax(int num){
+      Node node = root; 
+        int maxNum = 0; 
+        for(int i = 31;i>=0;i--) {
+            int bit = (num >> i) & 1; 
+            if(node.contain(1 - bit)) {
+                maxNum = maxNum | (1<<i);
+                node = node.get( 1 - bit); 
+            }
+            else {
+                node = node.get(bit); 
+            }
         }
+        return maxNum; 
+    }
+}
 
-        int[] ans = new int[queries.length];
+class Solution {
+    public int[] maximizeXor(int[] nums, int[][] queries) {
+        Trie obj = new Trie();
+        int q[][] = new int[queries.length][3];
+        int ans[]= new int[queries.length];
+        for(int i=0;i<q.length;i++){
+            q[i][0] =queries[i][0];
+            q[i][1] =queries[i][1];
+            q[i][2] =i;
+        }
         Arrays.sort(nums);
-        Arrays.sort(queries, (o1, o2) -> o1[1]-o2[1]);
+        Arrays.sort(q,(a,b)->a[1]-b[1]);
+       int idx=0;
+         for(int i=0;i<q.length;++i){
 
-
-        for(int i=0;i<queries.length;++i){
-
-            while(idx<n && nums[idx]<=queries[i][1]){
-                add(nums[idx]);
+            while(idx<nums.length && nums[idx]<=q[i][1]){
+                obj.insert(nums[idx]);
                 idx++;
             }
-            if(nums[0]>queries[i][1])
-                ans[queries[i][2]] = -1;
+            if(nums[0]>q[i][1])
+                ans[q[i][2]] = -1;
             else 
-                ans[queries[i][2]] = max_ans(queries[i][0]);
+                ans[q[i][2]] = obj.getMax(q[i][0]);
         }
 
         return ans;
 
-    }
-
-    public void add(int num){
-        Trie curNode = root;
-        for(int i = 31; i >= 0; i --) {
-            int curBit = (num >>> i) & 1;
-            if(curNode.children[curBit] == null) {
-                curNode.children[curBit] = new Trie();
-            }
-            curNode = curNode.children[curBit];
-        }
-    }
-
-
-    public int max_ans(int num){
-        Trie curNode = root;
-        int curSum = 0;
-        for(int i = 31; i >= 0; i --) {
-            int curBit = (num >>> i) & 1;
-            if(curNode ==null || curNode.children==null)
-                break;
-            if(curNode.children[curBit ^ 1] != null) {
-                curSum += (1 << i);
-                curNode = curNode.children[curBit ^ 1];
-            }else {
-                curNode = curNode.children[curBit];
-            }
-        }
-
-        return curSum;
+        
     }
 }
