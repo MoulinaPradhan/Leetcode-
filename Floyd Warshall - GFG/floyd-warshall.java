e2ee8cc0b8affc33d3c93916cfce831712273744
Moulina Pradhan <pmoulina76@gmail.com>
2022-06-24 16:18:40 +0530

    Added solution - LeetHub

--- Floyd Warshall - GFG/floyd-warshall.java
+++ Floyd Warshall - GFG/floyd-warshall.java
@@ -39,16 +39,21 @@ class Solution
 {
     public void shortest_distance(int[][] matrix)
     {
-         for(int k=0; k<matrix.length; k++){
-          for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix.length; j++){
-               if(matrix[i][k] != -1 && matrix[k][j] != -1 ){
-                  if(matrix[i][k]+matrix[k][j] < matrix[i][j] || matrix[i][j]==-1)
-                     matrix[i][j] = matrix[i][k]+matrix[k][j];
-               }
-                   
-            }
-          }
-        }
-    }
+      int n = matrix.length;
+	   
+	 
+	   for(int k=0;k<n;k++)
+	   {
+	       for(int i=0;i<n;i++)
+	       {
+	           for(int j=0;j<n;j++)
+	           {
+	               if(matrix[i][k]!=-1 && matrix[k][j]!=-1 && (matrix[i][j] > matrix[i][k] + matrix[k][j] || matrix[i][j]==-1))
+	               {
+	                   matrix[i][j] = matrix[i][k] + matrix[k][j];
+	               }
+	           }
+	       }
+	   }
+	}
 }
