0b1ece15a6884ed033d6dfce15c85b0cdbc0f400
Moulina Pradhan <pmoulina76@gmail.com>
2022-06-29 20:56:30 +0530

    Time: 1154 ms (5.00%), Space: 337.6 MB (37.84%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
@@ -1,25 +1,48 @@
 class Solution {
- private List<List<Integer>> ans = new ArrayList<>();
-    
-    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        for (List<Integer> c : connections) {
-            graph.computeIfAbsent(c.get(0), (k -> new ArrayList<Integer>())).add(c.get(1));
-            graph.computeIfAbsent(c.get(1), (k -> new ArrayList<Integer>())).add(c.get(0));
+    List<List<Integer>> ans = new ArrayList<>();
+    public List<List<Integer>> criticalConnections(int n,      List<List<Integer>> connect) {
+        // if(n==1) return connect;
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
         }
-        int[] timestamps = new int[n];
-        dfs(graph, 0, 0, 1, timestamps);
+         for(int i=0;i<connect.size();i++){
+          adj.get(connect.get(i).get(0)).add(connect.get(i).get(1));
+              adj.get(connect.get(i).get(1)).add(connect.get(i).get(0));
+        }
+        boolean vis[] = new boolean[adj.size()];
+int tin[] = new int[adj.size()];
+int low[] = new int[adj.size()];
+
+int timmer =0;
+for(int i=0;i<adj.size();i++){
+if(!vis[i]) 
+dfs(i,-1,vis,timmer,tin,low,adj);
+}
+
         return ans;
     }
-    
-    private int dfs(Map<Integer, List<Integer>> graph, int curr, int parent, int currTimestamp, int[] timestamps) {
-        timestamps[curr] = currTimestamp;
-        for (int nextNode : graph.getOrDefault(curr, new ArrayList<Integer>())) {
-            if (nextNode == parent) continue;
-            if (timestamps[nextNode] > 0) timestamps[curr] = Math.min(timestamps[curr], timestamps[nextNode]);
-            else timestamps[curr] = Math.min(timestamps[curr], dfs(graph, nextNode, curr, currTimestamp + 1, timestamps));
-            if (currTimestamp < timestamps[nextNode]) ans.add(Arrays.asList(curr, nextNode));
-        }
-        return timestamps[curr];
+    public void dfs(int node,int parent, boolean vis[], int timmer,int []tin, int low[], List<List<Integer> > adj){
+    vis[node] = true;
+    tin[node] = low[node]= timmer++;
+    for(Integer it: adj.get(node)){
+        if(it==parent) continue;
+if(!vis[it]){
+dfs(it,node,vis,timmer,tin,low,adj);
+low[node] = Math.min(low[it], low[node]);
+if(low[it] > tin[node]){
+    List<Integer> list = new ArrayList<>();
+    list.add(it);
+    list.add(node);
+    ans.add(list);
+    System.out.println(it+" "+node);
+}
+}else{
+    low[node] = Math.min(low[node], tin[it]);
+}
     }
+
+} 
+
+
 }
