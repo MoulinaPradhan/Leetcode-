dd79bd6972707d11a3992712c8dd77ca347e2fea
Moulina Pradhan <pmoulina76@gmail.com>
2022-06-30 12:42:57 +0530

    Added solution - LeetHub

--- Floyd Warshall - GFG/floyd-warshall.java
+++ Floyd Warshall - GFG/floyd-warshall.java
@@ -41,19 +41,14 @@ class Solution
     {
       int n = matrix.length;
 	   
-	 
-	   for(int k=0;k<n;k++)
-	   {
-	       for(int i=0;i<n;i++)
-	       {
-	           for(int j=0;j<n;j++)
-	           {
-	               if(matrix[i][k]!=-1 && matrix[k][j]!=-1 && (matrix[i][j] > matrix[i][k] + matrix[k][j] || matrix[i][j]==-1))
-	               {
-	                   matrix[i][j] = matrix[i][k] + matrix[k][j];
-	               }
-	           }
-	       }
-	   }
+	for(int k=0;k<n;k++){
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<n;j++){
+	            if(matrix[i][k] !=-1 && matrix[k][j] !=-1 &&(matrix[i][j] >matrix[i][k] +matrix[k][j] || matrix[i][j] ==-1 ))
+	     
+	     matrix[i][j] =matrix[i][k] +matrix[k][j];
+	        }
+	    }
 	}
+    }
 }
