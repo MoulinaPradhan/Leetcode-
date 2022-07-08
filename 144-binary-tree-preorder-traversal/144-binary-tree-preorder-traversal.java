7083913d12f7102a5c56105e339cf81ab945a4fd
Moulina Pradhan <pmoulina76@gmail.com>
2022-07-08 10:18:08 +0530

    Time: 1 ms (49.67%), Space: 42.4 MB (31.25%) - LeetHub

---
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode curr) {
+     
+        List<Integer> preOrder = new ArrayList < Integer > ();
+        if (curr == null)
+            return preOrder;
+
+        Stack <TreeNode > s = new Stack < > ();
+        s.push(curr);
+
+        while (!s.isEmpty()) {
+            TreeNode topNode = s.peek();
+            preOrder.add(topNode.val);
+            s.pop();
+            if (topNode.right != null)
+                s.push(topNode.right);
+            if (topNode.left != null)
+                s.push(topNode.left);
+        }
+        return preOrder;
+    }
+}
